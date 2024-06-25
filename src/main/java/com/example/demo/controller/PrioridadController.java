2989d372ba9d88af43fd4fe0f0607ee5541010d8
Crhistian Ninalaya <crhistianninalaya@gmail.com>
2024-06-24 20:07:39 -0500

    Listado de chats

--- src/main/java/com/example/demo/controller/PrioridadController.java
+++ src/main/java/com/example/demo/controller/PrioridadController.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.entity.ChatEntity;
 import com.example.demo.entity.PrioridadEntity;
+import com.example.demo.repository.ChatRepository;
 import com.example.demo.repository.PrioridadRepository;
 
 @Controller
@@ -17,10 +19,18 @@ public class PrioridadController {
 	@Autowired
 	private PrioridadRepository prioridadRepository;
 	
+	@Autowired
+	private ChatRepository chatRepository;
+	
 	@GetMapping("/chat")
 	public String showPrioridad(Model model) {
 		List<PrioridadEntity> lstPrioridad = prioridadRepository.findAll();
 		model.addAttribute("lstPrioridad",lstPrioridad);
+		
+		
+		List<ChatEntity> lstChat = chatRepository.findAll();
+		model.addAttribute("lstChat",lstChat);
+		
 		return "chat";
 	}
 
